f7968a1353aadbd7d1c4a0342c899ae2792dfdaf
JamesRickards-Canada <james.rickards@colorado.edu>
2024-01-21 22:20:42 -0700

    Finished with thickenedcurvatures, but method not yet tested (segfault currently).

--- aprime.c
+++ aprime.c
@@ -7,7 +7,7 @@
 #include "aprime.h"
 
 /*STATIC DECLARATIONS*/
-static void thickenedcurvatures_execute(long x[], long res[], long nres, long Bmin, long Bmax)
+static void thickenedcurvatures_execute(long x[], long res[], long nres, long Bmin, long Bmax, GEN vorig);
 static int sisprime(long p);
 
 /*MAIN BODY*/
@@ -38,19 +38,19 @@ thickenedcurvatures(GEN v, GEN B)
   long res[nres];
   for (i = 1; i <= nres; i++) res[i - 1] = itos(gel(modres, i));/*Move to C vector.*/
   long newv[4];
-  long iodd = 0, ieven = 2, i;
+  long iodd = 0, ieven = 2;
   for (i = 1; i <= 4; i++) {/*Make the first two odd.*/
-    if (Mod2(gel(v, i))) { newv[iodd] = itos(gel(v, i)); iodd++; }
-    else { newv[ieven] = itos(gel(v, i)); ieven++; }
+    if (Mod2(gel(v, i))) newv[iodd++] = itos(gel(v, i));
+    else newv[ieven++] = itos(gel(v, i));
   }
   if (!sisprime(newv[0]) && !sisprime(newv[1])) pari_err_TYPE("At least one of the odd numbers must be prime", v);
-  thickenedcurvatures_execute(newv, res, nres, Bmin, Bmax);
+  thickenedcurvatures_execute(newv, res, nres, Bmin, Bmax, v);
   set_avma(av);
 }
 
 /*Executes thickenedcurvatures. Assumes the first two entries of v are odd, with at least one positive prime.*/
 static void
-thickenedcurvatures_execute(long x[], long res[], long nres, long Bmin, long Bmax)
+thickenedcurvatures_execute(long x[], long res[], long nres, long Bmin, long Bmax, GEN vorig)
 {
   long Base = Bmin - (Bmin % 24);/*We want to start at a multiple of 24 to not ruin the mod stuff.*/
   long classmax = (Bmax - Base)/ 24 + 1, i;/*Maximal number of curvatures found in each class.*/
@@ -77,19 +77,15 @@ thickenedcurvatures_execute(long x[], long res[], long nres, long Bmin, long Bma
     printf("Insufficient memory to allocate to store the swaps.\n");
     exit(1);
   }
-  int *primes = (int **)pari_calloc(maxdepth * sizeof(int));/*1 if only x[0] is prime, 2 if only x[1] is prime, 3 if both are prime.*/
+  int *primes = (int *)pari_calloc(maxdepth * sizeof(int));/*1 if only x[0] is prime, 2 if only x[1] is prime, 3 if both are prime.*/
   if (!primes) {
     printf("Insufficient memory to allocate to store which indices are prime.\n");
     exit(1);
   }
-  for (i = 0; i < maxdepth; i++) {
-    swaps[i] = -1;/*Initialize to all -1's*/
-  }
+  for (i = 0; i < maxdepth; i++) swaps[i] = -1;/*Initialize to all -1's*/
   if (sisprime(x[0])) primes[0]++;
-  if (sisprime(x[1])) primes[1] += 2;
-  
-  
-  for (i = swaps[1] + 1; i < 4; i++) {/*Do the first curvatures.*/
+  if (sisprime(x[1])) primes[0] += 2;
+  for (i = 0; i < 4; i++) {/*Do the first curvatures.*/
     if (x[i] < Bmin || x[i] > Bmax) continue;
     long shifted = x[i] - Base;
     long b = shifted % 24;
@@ -97,132 +93,92 @@ thickenedcurvatures_execute(long x[], long res[], long nres, long Bmin, long Bma
     rclass[b][a]++;
   }
   long ind = 1;/*Which depth we are working at.*/
-  if (!sym) {/*Symmetries to worry about. More efficient to do this way, since we don't need to check for symmetries ever otherwise.*/
-    while (ind > 0) {/*We are coming in trying to swap this circle out.*/
-      int cind = ++swaps[ind];/*Increment the swapping index.*/
-      if (cind == 4) {/*Overflowed, go back.*/
-        swaps[ind] = -1;
-        ind--;
-        if (ind < sym) sym = 0;/*We moved past the first index swapping 3, so worry about symmetries again.*/
-        continue;
+  long v[4] = {x[0], x[1], x[2], x[3]};/*Initial quadruple.*/
+  swaps[0] = 0;/*This value does not matter, but we set it to 0 to not create a problem.*/
+  depthseq[0] = 0;/*Same thing here.*/
+  while (ind > 0) {/*We are coming in trying to swap this circle out.*/
+    int cind = ++swaps[ind];/*Increment the swapping index.*/
+    if (cind == 4) {/*Overflowed, go back.*/
+      swaps[ind] = -1;
+      ind--;
+      v[swaps[ind]] = depthseq[ind];/*Update our v backwards to the correct thing.*/
+      continue;
+    }
+    long lastind = ind - 1;
+    if (cind == swaps[lastind]) continue; /*Same thing twice, so skip it.*/
+    if (!cind && primes[lastind] == 1) continue;/*Swapping out the only prime, not allowed!*/
+    if (cind == 1 && primes[lastind] == 2) continue;/*Swapping out the only prime, not allowed!*/
+    long apbpc = 0;/*Now we can reasonably try a swap.*/
+    for (i = 0; i < cind; i++) apbpc += v[i];
+    for (i = cind + 1; i < 4; i++) apbpc += v[i];
+    long newc = (apbpc << 1) - v[cind];/*2(a+b+c)-d, the new curvature.*/
+    if (newc > Bmax) continue;/*Too big! go back.*/
+    long shifted = newc - Base;
+    if (shifted >= 0) {
+      long b = shifted % 24;
+      long a = shifted / 24;/*shifted = 24a + b. b gives the residue block, and a gives the place to insert it.*/
+      rclass[b][a]++;
+    }
+    depthseq[ind] = v[cind];/*Store the value we swapped away from.*/
+    v[cind] = newc;/*Update v*/
+    switch (cind) {/*Update primes*/
+      case 0:/*Check if prime*/
+        if (sisprime(newc)) primes[ind] = 3;/*1st index must be prime as we swapped the 0th*/
+        else primes[ind] = 2;
+        break;
+      case 1:/*Check if prime*/
+        if (sisprime(newc)) primes[ind] = 3;/*0th index must be prime as we swapped the 1st*/
+        else primes[ind] = 1;
+        break;
+      default:
+        primes[ind] = primes[lastind];/*Odd numbers did not change.*/
+    }
+    ind++;
+    if (ind == maxdepth) {/*We are going too deep, must pari_reallocate the storage location.*/
+      long newdepth = maxdepth << 1;/*Double it.*/
+      depthseq = pari_realloc(depthseq, newdepth * sizeof(long));
+      if (!depthseq) {
+        printf("Insufficient memory to reallocate the depth sequence.\n");
+        exit(1);
       }
-      long lastind = ind - 1;
-      if (cind == swaps[lastind]) continue; /*Same thing twice, so skip it.*/
-      if (!sym) {/*Worry about symmetries.*/
-        if (cind == 0) continue;/*We skip the first one.*/
-        else if (cind == 2) sym = ind + 1;/*First time we swap out the third one, eliminating symmetries further on in this branch.*/
+      swaps = pari_realloc(swaps, newdepth * sizeof(int));
+      if (!swaps) {
+        printf("Insufficient memory to reallocate the swaps.\n");
+        exit(1);
       }
-      long apbpc = 0;/*Now we can reasonably try a swap.*/
-      for (i = 0; i < cind; i++) apbpc += depthseq[lastind][i];
-      for (i = cind + 1; i < 4; i++) apbpc += depthseq[lastind][i];
-      long newc = (apbpc << 1) - depthseq[lastind][cind];/*2(a+b+c)-d, the new curvature.*/
-      if (newc > Bmax) {/*Too big! go back.*/
-        if (ind < sym) sym = 0;/*Tried flipping out of symmetry here but it's too big.*/
-        continue;
+      for (i = maxdepth; i < newdepth; i++) swaps[i] = -1;
+      primes = pari_realloc(primes, newdepth * sizeof(int));
+      if (!primes) {
+        printf("Insufficient memory to reallocate the primes.\n");
+        exit(1);
       }
-      long shifted = newc - Base;
-      if (shifted >= 0) {
-        long b = shifted % 24;
-        long a = shifted / 24;/*shifted = 24a + b. b gives the residue block, and a gives the place to insert it.*/
-        rclass[b][a]++;
-      }
-      for (i = 0; i < cind; i++) depthseq[ind][i] = depthseq[lastind][i];
-      depthseq[ind][cind] = newc;
-      for (i = cind + 1; i < 4; i++) depthseq[ind][i] = depthseq[lastind][i];/*Add the tuple in.*/
-      ind++;
-      if (ind == maxdepth) {/*We are going too deep, must pari_reallocate the storage location.*/
-        long newdepth = maxdepth << 1;/*Double it.*/
-        depthseq = pari_realloc(depthseq, newdepth * sizeof(long *));
-        if (!depthseq) {
-          printf("Insufficient memory to pari_reallocate the depth sequence.\n");
-          exit(1);
-        }
-        swaps = pari_realloc(swaps, newdepth * sizeof(int));
-        if (!swaps) {
-          printf("Insufficient memory to pari_reallocate the swaps.\n");
-          exit(1);
-        }
-        for (i = maxdepth; i < newdepth; i++) {
-          depthseq[i] = (long *)pari_malloc(sizeof(long) << 2);
-          swaps[i] = -1;
-        }
-        maxdepth = newdepth;
-      }
-    } 
-  }
-  else {/*No symmetry to worry about.*/
-    while (ind > 0) {/*We are coming in trying to swap this circle out.*/
-      int cind = ++swaps[ind];/*Increment the swapping index.*/
-      if (cind == 4) {/*Overflowed, go back.*/
-        swaps[ind] = -1;
-        ind--;
-        continue;
-      }
-      long lastind = ind - 1;
-      if (cind == swaps[lastind]) continue; /*Same thing twice, so skip it.*/
-      long apbpc = 0;/*Now we can reasonably try a swap.*/
-      for (i = 0; i < cind; i++) apbpc += depthseq[lastind][i];
-      for (i = cind + 1; i < 4; i++) apbpc += depthseq[lastind][i];
-      long newc = (apbpc << 1) - depthseq[lastind][cind];/*2(a+b+c)-d, the new curvature.*/
-      if (newc > Bmax) continue;/*Too big! go back.*/
-      long shifted = newc - Base;
-      if (shifted >= 0) {
-        long b = shifted % 24;
-        long a = shifted / 24;/*shifted = 24a + b. b gives the residue block, and a gives the place to insert it.*/
-        rclass[b][a]++;
-      }
-      for (i = 0; i < cind; i++) depthseq[ind][i] = depthseq[lastind][i];
-      depthseq[ind][cind] = newc;
-      for (i = cind + 1; i < 4; i++) depthseq[ind][i] = depthseq[lastind][i];/*Add the tuple in.*/
-      ind++;
-      if (ind == maxdepth) {/*We are going too deep, must pari_reallocate the storage location.*/
-        long newdepth = maxdepth << 1;/*Double it.*/
-        depthseq = pari_realloc(depthseq, newdepth * sizeof(long *));
-        if (!depthseq) {
-          printf("Insufficient memory to pari_reallocate the depth sequence.\n");
-          exit(1);
-        }
-        swaps = pari_realloc(swaps, newdepth * sizeof(int));
-        if (!swaps) {
-          printf("Insufficient memory to pari_reallocate the swaps.\n");
-          exit(1);
-        }
-        for (i = maxdepth; i < newdepth; i++) {
-          depthseq[i] = (long *)pari_malloc(sizeof(long) << 2);
-          swaps[i] = -1;
-        }
-        maxdepth = newdepth;
-      }
+      maxdepth = newdepth;
     }
   }
   /*Time to free some of the allocated memory.*/
+  pari_free(primes);
   pari_free(swaps);
-  for (i = 0; i < maxdepth; i++) pari_free(depthseq[i]);
   pari_free(depthseq);
-  if (tofile) curvs_tofile(rclass, Bmin, Bmax, classmax, v, modres);
-  if (tofile % 2) {
-    for (i = 0; i < lenr; i++) pari_free(rclass[res[i]]);
-    pari_free(rclass);/*The last thing to free*/
-    return gc_const(av, gen_0);
+  /*Save the curvature counts to file.*/
+  if( !pari_is_dir("curvcounts")) {
+    int s = system("mkdir -p curvcounts");
+    if (s == -1) pari_err(e_MISC, "ERROR CREATING DIRECTORY curvcounts");
   }
-  set_avma(av);/*Now we make it into a Vecsmall*/
-  long maxncur = classmax * lenr + 1, j;
-  GEN curvs = vecsmalltrunc_init(maxncur);
-  GEN freqs = vecsmalltrunc_init(maxncur);
-  long n = Base - 24;
-  for (i = 0; i < classmax; i++) {
-    n += 24;/*n = Base + 24i*/
-    for (j = 0; j < lenr; j++) {
-      long n1 = n + res[j];
-      if (n1 < Bmin || n1 > Bmax) continue;/*Too big/small*/
-      if (!rclass[res[j]][i]) continue;/*Does not occur*/
-      vecsmalltrunc_append(curvs, n1);
-      vecsmalltrunc_append(freqs, rclass[res[j]][i]);
+  char *filestart = stack_sprintf("curvcounts/%Pd_%Pd_%Pd_%Pd_%ld-to-%ld_res-", gel(vorig, 1), gel(vorig, 2), gel(vorig, 3), gel(vorig, 4), Bmin, Bmax);
+  for (i = 0; i < nres; i++) {
+    long r = res[i];/*The residue.*/
+    char *thisfile = stack_sprintf("%s%ld.dat", filestart, r);/*The file name.*/
+    FILE *f = fopen(thisfile, "w");/*Created the output file f*/
+    long firstind = 0;
+    if (Bmin > (Base + r)) firstind = 1;/*Where to start, since we start at Base not Bmin we want to ignore the first one sometimes.*/
+    long lastind = classmax - 1, j;
+    while ((Base + 24 * lastind + r) > Bmax) lastind--;/*Where to end it.*/
+    for (j = firstind; j <= lastind; j++) {
+      pari_fprintf(f, "%d\n", rclass[i][j]);
     }
-  }
-  for (i = 0; i < lenr; i++) pari_free(rclass[res[i]]);
+  } 
+  for (i = 0; i < nres; i++) pari_free(rclass[res[i]]);
   pari_free(rclass);/*The last thing to free*/
-  return gerepilecopy(av, mkvec2(curvs, freqs));
 }
 
 /*Checks if p is prime but allows for long, not just ulong, and returns 0 if p<=0 (even if -p is prime).*/
--- aprime.gp
+++ aprime.gp
@@ -1,7 +1,7 @@
 \r apol
 
 print("\n\nType '?aprime' for help.\n\n");
-addhelp(aprime, "This package is use for...");
+addhelp(aprime, "This package is used for computing thickened prime curvature components of Apollonian circle packings. Installed methods:\n\tthickenedcurvatures.");
 parigp_version = version();
 aprime_library = strprintf("./libaprime-%d-%d-%d.so", parigp_version[1], parigp_version[2], parigp_version[3]);
 
